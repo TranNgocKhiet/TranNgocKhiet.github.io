b6a4c62c2617674b15e6a5a7f55f6397aee77450
Slayer <slayersilver2005@gmail.com>
2025-06-05 01:33:20 +0700

    change

---
+++ java_project/hiv-treatment-support-system/backend/src/main/java/backend/doctorprofile/dto/CreateDoctorProfileRequest.java
@@ -0,0 +1,11 @@
+package backend.doctorprofile.dto;
+
+public record CreateDoctorProfileRequest(
+    String licenseNumber,
+    String startYear,
+    String qualifications,
+    String background,
+    String biography,
+    int userId
+){   
+}
